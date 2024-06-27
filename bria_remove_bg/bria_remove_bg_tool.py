a7da7d1d70c9bb8637bfa56bc3d5903d4117bc38
blessedcoolant <54517381+blessedcoolant@users.noreply.github.com>
2024-06-27 13:36:08 +0530

    fix: Use Invoke's new internal model cache / downloader API

--- bria_remove_bg/bria_remove_bg_tool.py
+++ bria_remove_bg/bria_remove_bg_tool.py
@@ -1,28 +1,14 @@
 import pathlib
-from typing import Any, Literal, Optional
 
 import numpy as np
 import torch
 from invokeai.app.services.config.config_default import get_config
-from invokeai.app.util.download_with_progress import download_with_progress_bar
-from invokeai.backend.util.devices import choose_torch_device
 from PIL import Image
 
 from .bria_rmbg import BriaRMBG
 from .utils import postprocess_image, preprocess_image
 
 config = get_config()
-
-BRIA_RMBG_MODELS = {
-    "1.4": {
-        "url": "https://huggingface.co/briaai/RMBG-1.4/resolve/main/model.pth?download=true",
-        "local": "any/bria-rmbg/model.pth",
-    },
-    "Open RMBG": {
-        "url": "https://huggingface.co/schirrmacher/ormbg/resolve/main/models/ormbg.pth?download=true",
-        "local": "any/bria-rmbg/ormbg.pth",
-    },
-}
 
 
 def resize_image(image: Image.Image) -> Image.Image:
@@ -32,28 +18,19 @@ def resize_image(image: Image.Image) -> Image.Image:
     return image
 
 
-RMBG_MODEL_TYPES = Optional[Literal["1.4", "Open RMBG"]]
-
-
 class BriaRMBGTool:
-    def __init__(self) -> None:
-        self.network: Optional[BriaRMBG] = None
-        self.device = choose_torch_device()
-        self.current_model: RMBG_MODEL_TYPES = None
+    def __init__(self, model: BriaRMBG, device: torch.device) -> None:
+        self.network = model
+        self.device = device
 
-    def load_model(self, model_type: RMBG_MODEL_TYPES = "1.4"):
-        self.current_model = model_type
-        BRIA_RMBG_MODEL_PATH = pathlib.Path(config.models_path / BRIA_RMBG_MODELS[model_type]["local"])
-        if not BRIA_RMBG_MODEL_PATH.exists():
-            download_with_progress_bar(
-                "BRIA RMBG Checkpoint", BRIA_RMBG_MODELS[model_type]["url"], BRIA_RMBG_MODEL_PATH
-            )
+    @staticmethod
+    def load_model(model_path: pathlib.Path, device: torch.device) -> BriaRMBG:
+        network = BriaRMBG()
+        network.load_state_dict(torch.load(model_path, map_location=device))
+        network.eval()
+        return network
 
-        self.network = BriaRMBG()
-        self.network.load_state_dict(torch.load(BRIA_RMBG_MODEL_PATH, map_location=self.device))
-        self.network.eval()
-
-    def remove_background(self, image: Image.Image) -> Any:
+    def remove_background(self, image: Image.Image) -> Image.Image:
         model_size = [1024, 1024]
 
         image = image.convert("RGB") if image.mode != "RGB" else image  # Ensure image is RGB
@@ -66,7 +43,7 @@ class BriaRMBGTool:
         np_image = np.array(image)
 
         tensor_image = preprocess_image(np_image, model_size)
-        if self.device == "cuda":
+        if self.device.type == "cuda":
             tensor_image = tensor_image.cuda()
         bg_removed_tensor = self.network(tensor_image)
         tensor_image = postprocess_image(bg_removed_tensor[0][0], (image_height, image_width))
--- node/__init__.py
+++ node/__init__.py
@@ -1,11 +1,18 @@
-from typing import Literal
+import pathlib
+from typing import Literal, Optional
 
 from invokeai.app.invocations.primitives import ImageField, ImageOutput
+from invokeai.backend.util.devices import TorchDevice
 from invokeai.invocation_api import BaseInvocation, InputField, InvocationContext, WithMetadata, invocation
 
-from ..bria_remove_bg.bria_remove_bg_tool import RMBG_MODEL_TYPES, BriaRMBGTool
+from ..bria_remove_bg.bria_remove_bg_tool import BriaRMBGTool
 
-bria_remover = None
+BRIA_RMBG_MODELS = {
+    "1.4": "https://huggingface.co/briaai/RMBG-1.4/resolve/main/model.pth",
+    "Open RMBG": "https://huggingface.co/schirrmacher/ormbg/resolve/main/models/ormbg.pth",
+}
+
+RMBG_MODEL_TYPES = Optional[Literal["1.4", "Open RMBG"]]
 
 
 @invocation(
@@ -21,16 +28,18 @@ class BriaRemoveBackgroundInvocation(BaseInvocation, WithMetadata):
     image: ImageField = InputField(description="The image to crop")
     model: RMBG_MODEL_TYPES = InputField(default="1.4", description="The model to use for background removal")
 
+    def load_model(self, context):
+        def loader(model_path: pathlib.Path):
+            return BriaRMBGTool.load_model(model_path, TorchDevice.choose_torch_device())
+
+        with context.models.load_remote_model(source=BRIA_RMBG_MODELS[self.model], loader=loader) as model:
+            bria_remover = BriaRMBGTool(model, TorchDevice.choose_torch_device())
+            return bria_remover
+
     def invoke(self, context: InvocationContext) -> ImageOutput:
-        global bria_remover
         image = context.images.get_pil(self.image.image_name)
 
-        if not isinstance(bria_remover, BriaRMBGTool):
-            bria_remover = BriaRMBGTool()
-
-        if bria_remover.network is None or bria_remover.current_model != self.model:
-            bria_remover.load_model(self.model)
-
+        bria_remover = self.load_model(context)
         bg_removed_image = bria_remover.remove_background(image)
 
         image_dto = context.images.save(image=bg_removed_image)
